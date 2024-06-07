631bd40e7a2153fe7552a0a7cc657e240cca69c2
Sam Truesdale <samtruesdale10@gmail.com>
2024-06-07 19:34:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.165.0/build/three.module.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 // import * as THREE from 'three';
 
 // import mp4 from './background.mp4';
