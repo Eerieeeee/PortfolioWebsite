8557596fb913e69bf903b061079b9602aaa86706
Sam Truesdale <samtruesdale10@gmail.com>
2024-06-10 20:44:53 +0100

    raw file links

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ renderer.toneMappingExposure = 1;
 // ADDING VIDEO TEXTURE TO PLANE
 const video = document.getElementById('TV-Texture'); //define video as a const
 
-video.src="Images/TV-Texture.mp4";
+video.src="https://github.com/Eerieeeee/PortfolioWebsite/raw/main/Videos/TV-Texture.mp4";
 video.load();
 video.play();
 const texture = new THREE.VideoTexture(video); //call the var video as a texture
@@ -114,7 +114,7 @@ scene.add(imageObject);
 // //FOOD COLLAGE VIDEO
 const foodVideo = document.getElementById('FoodCollage'); //define video as a const
 
-foodVideo.src="Images/FoodCollage.mp4";
+foodVideo.src="https://github.com/Eerieeeee/PortfolioWebsite/raw/main/Videos/FoodCollage.mp4";
 foodVideo.load();
 foodVideo.play();
 const foodTexture = new THREE.VideoTexture(foodVideo); //call the var video as a texture
