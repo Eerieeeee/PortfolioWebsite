2f001858199973d8d09729b398c0adce8ac0bdef
Sam Truesdale <samtruesdale10@gmail.com>
2024-06-10 17:16:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // import mp4 from './background.mp4';
 
