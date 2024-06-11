2d2962f8a444970c31f8a85428f437dba029e913
Sam Truesdale <samtruesdale10@gmail.com>
2024-06-11 22:01:00 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -72,7 +72,7 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 
     modelLoader.load(
-        './public/Homepage.glb',
+        '/public/Homepage.glb',
         
         function(gltf) {
             scene.add( gltf.scene );
@@ -93,7 +93,7 @@ renderer.toneMappingExposure = 1;
 // ADDING VIDEO TEXTURE TO PLANE
 const video = document.getElementById('TV-Texture'); //define video as a const
 
-video.src="./public/TV-Texture.mp4";
+video.src="/public/TV-Texture.mp4";
 video.load();
 video.play();
 const texture = new THREE.VideoTexture(video); //call the var video as a texture
@@ -115,7 +115,7 @@ scene.add(imageObject);
 // //FOOD COLLAGE VIDEO
 const foodVideo = document.getElementById('FoodCollage'); //define video as a const
 
-foodVideo.src="./public/FoodCollage.mp4";
+foodVideo.src="/public/FoodCollage.mp4";
 foodVideo.load();
 foodVideo.play();
 const foodTexture = new THREE.VideoTexture(foodVideo); //call the var video as a texture
